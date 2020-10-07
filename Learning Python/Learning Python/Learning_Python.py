080f9c18761a6993fd1440c0b4384c43cdf4f107
niteshshyam <72507352+niteshshyam@users.noreply.github.com>
2020-10-07 14:54:15 +0100

    Add files via upload

---
+++ Learning Python/Learning Python/Learning_Python.py
@@ -0,0 +1,14 @@
+multiple = int(input("Enter Number"))
+amount = int(input("Enter Amount"))
+smultiple = str(multiple)
+samount = str(amount)
+n = 0
+while n< amount + 1:
+	answer = multiple * n
+	print(smultiple, "x", n, "=", answer)
+	n = n+1
+
+
+
+
+
---
+++ Learning Python/Learning Python/module1.py
@@ -0,0 +1,55 @@
+#Calculator
+
+
+
+
+if input() == str("new"):
+    print("Options: \n  1. Addition \n  2. Subtraction \n  3. Multiplication \n  4. Division")
+    option = int(input("Enter an Option: "))
+    if option > 4:
+        print("Error! Enter Valid Option")
+        option = int(input("Option: "))
+        print("Selected Option: ", option)
+    if option == 1:
+            number1 = float(input("Enter A Number: "))
+            number2 = float(input("Enter Another Number: "))
+            ans= float(number1 + number2)
+            print("Ans: ", ans)
+    if input() == str("save"):
+        x = float(ans)
+        number3 = float(input("Enter A Number: "))
+        ans = float(x + number3)
+        print(ans)
+    if option == 2:
+        number1 = float(input("Enter A Number: "))
+        number2 = float(input("Enter Another Number: "))
+        ans= float(number1 - number2)
+        print("Ans: ", ans)
+    if input() == str("save"):
+        x = float(ans)
+        number3 = float(input("Enter A Number: "))
+        ans = float(x - number3)
+        print(ans)
+    if option == 3:
+        number1 = float(input("Enter A Number: "))
+        number2 = float(input("Enter Another Number: "))
+        ans= float(number1 * number2)
+        print("Ans: ", ans)
+    if input() == str("save"):
+        x = float(ans)
+        number3 = float(input("Enter A Number: "))
+        ans = float(x * number3)
+        print(ans)
+    if option == 4:
+        number1 = float(input("Enter A Number: "))
+        number2 = float(input("Enter Another Number: "))
+        ans= float(number1 / number2)
+        print("Ans: ", ans)
+    if input() == str("save"):
+        x = float(ans)
+        number3 = float(input("Enter A Number: "))
+        ans = float(x / number3)
+        print(ans)
+
+
+
