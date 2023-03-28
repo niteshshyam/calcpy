b6833a7a1e023e7280e596220ae56982eaa6d9da
Nitesh Shyam <nitesh.shyam@student.manchester.ac.uk>
2023-03-28 15:15:26 +0100

    Just found this after 3 years. My first ever python program and it fails to work properly, typical

--- Learning Python/Learning Python/module1.py
+++ Learning Python/Learning Python/module1.py
@@ -1,8 +1,5 @@
 #Calculator
 
-
-
-
 if input() == str("new"):
     print("Options: \n  1. Addition \n  2. Subtraction \n  3. Multiplication \n  4. Division")
     option = int(input("Enter an Option: "))
